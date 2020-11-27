586c4a94c1f664075f6b3ddf1b14d3804bc78cf4
tavolafourcade <tavolafourcade@gmail.com>
2020-11-26 19:38:10 -0500

    Tried to apply theme versions in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
+import { addParameters } from "@storybook/react";
+import { addDecorator } from "@storybook/react";
+import { withContexts } from "@storybook/addon-contexts/react";
+import { contexts } from "./contexts";
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+addParameters({
+  backgrounds: [
+    { name: "Default theme", value: "#ffffff", default: true },
+    { name: "Dark theme", value: "#050449" }
+  ]
+});
+
+addDecorator(withContexts(contexts));
